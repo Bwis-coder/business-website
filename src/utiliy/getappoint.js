5fa0ed753841bd62bff5e9ccb8854e63a5ab4b9d
Bwis <wisdomEzekiel9@gmail.com>
2026-06-06 10:13:19 +0100

    fix number

--- src/utiliy/getappoint.js
+++ src/utiliy/getappoint.js
@@ -1,5 +1,5 @@
 const bookAppointment = () => {
-  const phone = "2349037455456";
+  const phone = "2349026782582";
   const message = "Hi, I would like to book an appointment";
   const url = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
 
